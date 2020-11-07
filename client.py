7c5b78deefeb0d9fd95bd791abc97566245fae95
TevfikUlu <tevfik_uluocak@hotmail.com>
2020-11-07 21:52:05 +0300

    adder uart to TCP client

--- client.py
+++ client.py
@@ -1,11 +1,33 @@
+import serial
 import socket
 
 s = socket.socket()
 host = "192.168.1.104"
-port = 80
+port = 80 
+from time import sleep
+
+
 s.connect((host,port))
-s.send('randomData'.encode())
-data = ''
-data = s.recv(1024).decode()
-print (data)
-s.close
+ser = serial.Serial ("/dev/ttyS0", 115200)    #Open port with baud rate
+while True:
+    received_data = ser.read()              #read serial port
+    sleep(0.03)
+    data_left = ser.inWaiting()             #check for remaining byte
+    received_data += ser.read(data_left)
+    print (received_data)                   #print received data
+    ser.write(received_data)                #transmit data serially 
+    s.send(received_data.encode())
+
+''''
+import serial
+from time import sleep
+
+ser = serial.Serial ("/dev/ttyS0", 115200)    #Open port with baud rate
+while True:
+    received_data = ser.read()              #read serial port
+    sleep(0.03)
+    data_left = ser.inWaiting()             #check for remaining byte
+    received_data += ser.read(data_left)
+    print (received_data)                   #print received data
+    ser.write(received_data)                #transmit data serially 
+'''
